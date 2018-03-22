6a6bc1449602b2feab2eb22d73e5837b396cc4ef
unknown <Rayhanshuvo>
2018-03-22 20:05:16 +0600

    first commit

---
+++ app/src/main/java/com/example/rayhan/design1/ListItem.java
@@ -0,0 +1,24 @@
+package com.example.rayhan.design1;
+
+/**
+ * Created by Rayhan on 3/22/2018.
+ */
+
+public class ListItem {
+
+    private String head;
+    private String description;
+
+    public ListItem(String head, String description) {
+        this.head = head;
+        this.description = description;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
